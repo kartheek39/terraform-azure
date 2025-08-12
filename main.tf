b322d429a2bcb762cbe1aa71c4c089fe392a0a9b
kartheek bodigam <bablutech39@gmail.com>
2025-08-12 18:51:19 +0530

    initial commit

---
+++ main.tf
@@ -0,0 +1,58 @@
+
+resource "azurerm_resource_group" "practice_rg" {
+    name = "practice-rg"
+    location = "east-us-01"
+}
+
+resource "azurerm_virtual_network" "practice_vnet" {
+  name = "practice-vnet"
+  address_space = "10.10.0.0/16"
+  location = azurerm_resource_group.practice_rg.location
+  resource_group_name = azurerm_resource_group.practice_rg.name
+}
+
+resource "azurerm_subnet" "practice_subnet_pub" {
+  name = "practice-sub-pub"
+  resource_group_name = azurerm_resource_group.practice_rg.name
+  virtual_network_name = azurerm_virtual_network.practice_vnet.name
+  address_prefixes = "10.10.1.0/24"
+}
+
+resource "azurerm_network_interface" "practice_nic" {
+  name = "practice-nic-01"
+  location = azurerm_resource_group.practice_rg.location
+  resource_group_name = azurerm_resource_group.practice_rg.name
+  ip_configuration {
+    name = "internal"
+    subnet_id = azurerm_subnet.practice_subnet_pub.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+
+resource "azurerm_linux_virtual_machine" "practice_vm" {
+  name                = "practice-vm"
+  resource_group_name = azurerm_resource_group.practice_rg.name
+  location            = azurerm_resource_group.practice_rg.location
+  size                = "Standard_B2s"
+  admin_username      = "adminuser"
+  network_interface_ids = [
+    azurerm_network_interface.example.id,
+  ]
+
+  admin_ssh_key {
+    username   = "adminuser"
+    public_key = file("~/.ssh/id_rsa.pub")
+  }
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "0001-com-ubuntu-server-jammy"
+    sku       = "22_04-lts"
+    version   = "latest"
+  }
+}
